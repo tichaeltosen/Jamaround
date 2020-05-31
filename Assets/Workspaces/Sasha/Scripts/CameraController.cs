9dc62b790e92d67c12dfa709947185ee345d3b02
Sasha <sashamandel@gmail.com>
2020-05-30 18:12:23 -0700

    third person controller
    set up a scene with a third person controller

---
+++ Assets/Workspaces/Sasha/Scripts/CameraController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    Vector3 velocity;
+    
+    public float smoothTimeZ;
+
+    public float smoothTimeX;
+    
+    public float offsetZ = -20f;
+
+    public GameObject player;
+    
+    private Vector3 originPos;
+
+    private void Start()
+    {
+        originPos = transform.position;
+    }
+
+    void FixedUpdate()
+    {
+        float posX = Mathf.SmoothDamp(transform.position.x, player.transform.position.x, ref velocity.x, smoothTimeX);
+        float posZ = Mathf.SmoothDamp(transform.position.z, player.transform.position.z + offsetZ, ref velocity.z, smoothTimeZ);
+        
+        transform.position = new Vector3(posX, transform.position.y, posZ);
+    }
+
+
+
+}
---
+++ Assets/Workspaces/Sasha/Scripts/PlayerMovement.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    private PlayerInputActions inputActions;
+    private Vector2 movementInput;
+    [SerializeField]
+    private float moveSpeed = 10f;
+
+    private Vector3 inputDirection;
+    private Vector3 moveVector;
+    private Quaternion currentRotation;
+
+    void Awake()
+    {
+        inputActions = new PlayerInputActions();
+        inputActions.Player.Movement.performed += context => movementInput = context.ReadValue<Vector2>();
+    }
+
+    void FixedUpdate()
+    {
+        float h = movementInput.x;
+        float v = movementInput.y;
+
+        Vector3 targetInput = new Vector3(h, 0, v);
+
+        inputDirection = Vector3.Lerp(inputDirection, targetInput, Time.deltaTime * 10f);
+
+        Vector3 camForward = Camera.main.transform.forward;
+        Vector3 camRight = Camera.main.transform.right;
+        camForward.y = 0f;
+        camRight.y = 0f;
+        
+        Vector3 desiredDirection = camForward * inputDirection.z + camRight * inputDirection.x;
+
+        Move(desiredDirection);
+        Turn(desiredDirection);
+    
+    }
+
+    void Move(Vector3 desiredDirection)
+    {
+        moveVector.Set(desiredDirection.x, 0f, desiredDirection.z);
+        moveVector = moveVector * moveSpeed * Time.deltaTime;
+        transform.position += moveVector;
+    }
+
+    void Turn(Vector3 desiredDirection)
+    {
+        if((desiredDirection.x > 0.1 || desiredDirection.x < -0.1) || (desiredDirection.z > 0.1 || desiredDirection.z < -0.1))
+        {
+            currentRotation = Quaternion.LookRotation(desiredDirection);
+            transform.rotation = currentRotation;
+        }
+        else
+        transform.rotation = currentRotation;
+
+
+    }
+
+private void OnEnable()
+{
+    inputActions.Enable();
+}
+
+private void OnDisable()
+{
+    inputActions.Disable();
+}
+
+}
