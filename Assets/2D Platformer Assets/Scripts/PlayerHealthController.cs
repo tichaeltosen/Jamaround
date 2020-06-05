f6b89edaa9fa7a2443c1519dc63654501c44302e
tichaeltosen <mrosen86@gmail.com>
2020-06-04 19:59:53 -0700

    updates with 2D platformer tutorials

---
+++ Assets/2D Platformer Assets/Scripts/PlayerHealthController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealthController : MonoBehaviour
+{
+    public static PlayerHealthController instance;
+
+    public int currentHealth, maxHealth;
+
+    public float invincibleLength;
+    private float invincibleCounter;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (invincibleCounter > 0)
+        {
+            invincibleCounter -= Time.deltaTime;
+        }
+        
+    }
+
+    public void DealDamage()
+    {
+        if (invincibleCounter <= 0)
+        {
+            currentHealth--;
+
+            if (currentHealth <= 0)
+            {
+                currentHealth = 0; //error handling
+
+                gameObject.SetActive(false);
+
+            }
+            else
+            {
+                invincibleCounter = invincibleLength;
+            }
+
+            UIController.instance.UpdateHealthDisplay();
+
+        }
+    }
+}
